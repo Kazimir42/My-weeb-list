125c25ec0e3edaee5a364ba17a1a776388c3da93
kazi <champlonremii@gmail.com>
2022-04-16 10:52:43 +0200

    working on extension

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
 
 
-function searchAnimes(text)
-{
-    resultDom = document.getElementById('animesSearchResult');
-    axios.get('https://api.jikan.moe/v4/anime?q=' + text).then(resp => {
+/** BUTTON SEARCH ANIME */
+let btn = document.getElementById('searchButton');
+btn.addEventListener('click', function() {
+    searchAnimes(document.getElementById('searchInput').value)
+});
 
-        let datas = resp.data;
 
+/** INPUT SEARCH ANIME */
+let input = document.getElementById('searchInput');
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("searchButton").click();
+    }
+});
 
-        datas.data.forEach(function (item) {
-            resultDom.innerHTML +=
-                '<div class="">' +
-                item.title +
-                '</div>'
+function searchAnimes(text) {
+    let resultDom = document.getElementById('animesSearchResult');
+    resultDom.innerHTML = '';
 
+    const url = 'https://api.jikan.moe/v4/anime?q=' + text;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            let datas = data.data;
+
+            datas.forEach(function (item) {
+                resultDom.innerHTML +=
+                    '<div class="">' +
+                    item.title +
+                    '</div>'
+            });
         });
 
-    });
+}
+
+async function newSearch(text) {
+    try {
+        await searchAnimes(text);
+    } catch (err) {
+        console.log(err);
+    }
 }
