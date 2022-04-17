1f224e6d83203cf04eb3b7614c2965c911972ee1
kazi <champlonremii@gmail.com>
2022-04-17 15:40:39 +0200

    store anime in sync storage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@
 let homeButton = document.getElementById('homeButton');
 homeButton.addEventListener('click', function() {
     //HIDE ALL
-    document.getElementById('anime_div').style.display = 'none'
+    document.getElementById('anime_div').style.display = 'none';
+    document.getElementById('my_animes_div').style.display = 'none';
 
     //DISPLAY SEARCH
-    document.getElementById('search_div').style.display = 'initial'
+    document.getElementById('search_div').style.display = 'initial';
 });
+
+
+/** MY ANIMES BUTTON */
+let myAnimeButton = document.getElementById('myAnimeButton');
+myAnimeButton.addEventListener('click', function() {
+    //HIDE ALL
+    document.getElementById('anime_div').style.display = 'none';
+    document.getElementById('search_div').style.display = 'none';
+
+    //DISPLAY SEARCH
+    document.getElementById('my_animes_div').style.display = 'initial';
+});
+
+
 /** BUTTON SEARCH ANIME */
 let btn = document.getElementById('searchButton');
 btn.addEventListener('click', function() {
@@ -23,6 +38,22 @@ input.addEventListener("keyup", function(event) {
     }
 });
 
+/** BUTTON ADD EPISODE */
+let btnEpisode = document.getElementById('episodeButton');
+btnEpisode.addEventListener('click', function() {
+    AddOrUpdateAnime(document.getElementById('episodeInput').value, document.getElementById('animeId').value, document.getElementById('animeTitle').value)
+});
+
+
+/** INPUT ADD ANIME */
+let inputEpisode = document.getElementById('episodeInput');
+inputEpisode.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("episodeButton").click();
+    }
+});
+
 /** FUNCTION SEARCH LIST OF ANIMES */
 function searchAnimes(text) {
     let resultDom = document.getElementById('animesSearchResult');
@@ -36,6 +67,8 @@ function searchAnimes(text) {
 
             let datas = data.data;
             let itemsProcessed = 0;
+
+            console.log(datas)
 
             datas.forEach(function (item) {
                 itemsProcessed++;
@@ -80,7 +113,86 @@ function showAnime(mlId)
             console.log(anime)
             resultAnime.innerHTML += '<a href="'+anime.url+'" target="_blank" class="text-2xl font-semibold">'+anime.title+'</a>' +
                 '<img class="mx-auto" src="'+anime.images.jpg.image_url+'" />' +
-                '<p>Episodes : '+ anime.episodes +'</p>';
-
+                '<p>Episodes : '+ anime.episodes +'</p>' +
+                '<input type="hidden" id="animeId" value="'+anime.mal_id+'">' +
+                '<input type="hidden" id="animeTitle" value="'+anime.title+'">' +
+                '<br />';
         });
+}
+
+
+function AddOrUpdateAnime(episode, id, title)
+{
+
+    //CHECK IF STORAGE IS EMPTY
+    chrome.storage.sync.get(function(result){
+        if (Object.keys(result).length === 0 && result.constructor === Object)
+        {
+            //CREATE STORAGE VAR
+            console.log('empty')
+
+            let animes = [];
+            let anime = {
+                id: id,
+                title: title,
+                episode: episode
+            }
+            animes.push(anime)
+
+            chrome.storage.sync.set({animes}, function() {
+                console.log('add ' + anime);
+            });
+
+        }else{
+            //ADD TO STORAGE VARS
+            console.log('plein')
+            console.log(result)
+
+            let animes = result.animes;
+            let anime = {
+                id: id,
+                title: title,
+                episode: episode
+            }
+            animes.push(anime)
+
+
+            chrome.storage.sync.set({animes}, function() {
+                console.log('add ' + anime);
+            });
+
+        }
+    })
+
+    /*
+    //IF USER HAVE ANIMES
+    if (chrome.storage.sync.get())
+    {
+        chrome.storage.sync.get(['animes'], function(result) {
+            console.log('Value currently is ' + result.key);
+        });
+    }else { //ELSE
+        let anime = {
+            id: id,
+            title: title,
+            episode: episode
+        }
+
+        chrome.storage.sync.set({animes: [anime]}, function() {
+            console.log('add ' + anime);
+        });
+
+        console.log( chrome.storage.sync.get(null, function(items) {
+            var allKeys = Object.keys(items);
+            console.log(allKeys);
+        }));
+    }*/
+
+
+}
+
+
+function CreateAnimesStorage()
+{
+
 }
